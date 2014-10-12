27575ff8458434eb85ec50cabeb6e36c690527c8
Georgios Koloventzos <gk2409@columbia.edu>
2014-10-11 20:24:50 -0400

    checkpatch for accevt.c

--- flo-kernel/kernel/accevt.c
+++ flo-kernel/kernel/accevt.c
@@ -14,11 +14,11 @@ static LIST_HEAD(events_list);
 
 static int event_search(int event_id, struct list_head *head)
 {
-	struct list_head* position;
+	struct list_head *position;
 	int find = 0;
 	spin_lock(&eventlist_lock);
-	list_for_each(position, &events_list){
-		if (position.event_id == event_id){
+	list_for_each(position, &events_list) {
+		if (position.event_id == event_id) {
 			find = 1;
 			break;
 		}
@@ -31,34 +31,29 @@ int sys_accevt_create(struct acc_motion __user *acceleration)
 {
 	int num_events = 0;
 	struct motion_event *new_event;
-	struct list_head* position;
+	struct list_head *position;
 	new_event = kmalloc(sizeof(struct motion_event), GFP_KERNEL);
-	if (new_event == NULL) {
-		 errno = -ENOMEM;
-		 return -1;
-	}
+	if (new_event == NULL)
+		return -ENOMEM;
 	spin_lock(&eventlist_lock);
-	if (! list_empty(&events_list)) {
-		list_for_each(position, &events_list){
+	if (!list_empty(&events_list)) {
+		list_for_each(position, &events_list) {
 			++num_events;
 		}
 	}
 	new_event->event_id = ++num_events;
-	list_add(new_event,events_list);
+	list_add(new_event, events_list);
 	spin_unlock(&eventlist_lock);
 	return num_events;
 }
 
 int sys_accevt_wait(int event_id)
 {
-	struct list_head* position;
+	struct list_head *position;
 	int found;
 	found = event_search(event_id, events_list);
-	if (! found) {
-		errno = -ERROR;
-		return -1;
-	}
-
+	if (!found)
+		return -ENOMEM;
 }
 
 int sys_accevt_signal(struct dev_acceleration __user *acceleration)
